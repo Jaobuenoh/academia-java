58fdaf08998732882b58d625c7f1a2076a462c3c
Bruno Casagrande <bruno_casagrande@outlook.com.br>
2023-11-30 19:22:06 -0300

    adicionando paginas html

--- dashboard/src/main/java/com/demo/dashboard/security/AppSecurity.java
+++ dashboard/src/main/java/com/demo/dashboard/security/AppSecurity.java
@@ -1,4 +1,4 @@
-package com.demo.dashboard.controller;
+package com.demo.dashboard.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
